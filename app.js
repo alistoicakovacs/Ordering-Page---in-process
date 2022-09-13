6197a17f7257e630d44cf35f6dbea076bdaf0e8a
Ali Stoica Kovacs <78688064+alistoicakovacs@users.noreply.github.com>
2022-09-13 16:33:05 +0200

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const username = document.getElementById("username")
-const userEmail = document.getElementById("userEmail")
-const userPassword = document.getElementById("userPassword")
-const projektNummer = document.getElementById("projektnummer")
-
-
