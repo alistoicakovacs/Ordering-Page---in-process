f24e8ec8babbd22c9ed6a1dd7d7984b0803dba46
Ali Stoica Kovacs <78688064+alistoicakovacs@users.noreply.github.com>
2022-09-14 13:21:44 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,16 +14,16 @@ const user2 = new User("jcl", 1234)
 
 // login button 
 loginButton.addEventListener("click", function (element) {
-	if (username.value ===user1.username && password.value == user1.password) {
-		console.log("success");
-		document.querySelector(".login-container").classList.add("hidden");
-		document.querySelector(".item-container").classList.remove("hidden");
-	} else {
-		alert("Incorrect username or password");
-	}
+	// if (username.value ===user1.username && password.value == user1.password) {
+	// 	console.log("success");
+	// 	document.querySelector(".login-container").classList.add("hidden");
+	// 	document.querySelector(".item-container").classList.remove("hidden");
+	// } else {
+	// 	alert("Incorrect username or password");
+	// }
 	// //testing
-	// document.querySelector(".login-container").classList.add("hidden");
-	// document.querySelector(".item-container").classList.removeAtt("hidden");
+	document.querySelector(".login-container").classList.add("hidden");
+	document.querySelector(".item-container").classList.remove("hidden");
 	// users.forEach(function(element) {
 		
 	// 	if (username.value === element.username && password.value === element.password) {
@@ -34,7 +34,7 @@ loginButton.addEventListener("click", function (element) {
 	// }});
 });
 
-
+/////////////////////////////////////////////////
 // The list should contain the products from the JSON file that has to be converted from the xls list.
 // Login functionality has yet to be fixed and be set dynamic
 	// Username and Password should match for each individual user
@@ -43,3 +43,7 @@ loginButton.addEventListener("click", function (element) {
 
 // List items - filter 
 	// Show picture on hover
+
+const jsonObject = fetch('convertcsv.json')
+.then(response => response.json())
+.then(json => document.querySelector('.items').append(json[2].value))
