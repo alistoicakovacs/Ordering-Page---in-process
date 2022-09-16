99156ccf1304105a4040214e8005724598395332
Ali Stoica Kovacs <78688064+alistoicakovacs@users.noreply.github.com>
2022-09-16 14:35:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@ const loginButton = document.querySelector(".login-button");
 const username = document.querySelector(".user-input");
 const password = document.querySelector(".user-password");
 const items = document.querySelector(".items");
+const cartButton = document.querySelector('.cart-button')
+const dropdown = document.querySelectorAll(".dropdown");
+const itemsListContainer = document.querySelector(".item-list-container");
+const searchbox = document.getElementById("searchbox");
+const lieferantenDropdown = document.getElementById("lieferanten");
+const gewerkDropdown = document.getElementById("gewerk");
+const anwendungDropdown = document.getElementById("anwendung");
+const cart = document.querySelector('.cart')
+const cartContainer = document.querySelector('.cart-container')
+const divContainer=document.querySelector(".div-container")
+
 /////////////////////////////////////////////////
 // The list should contain the products from the JSON file that has to be converted from the xls list.
 // Login functionality has yet to be fixed and be set dynamic
@@ -14,6 +25,8 @@ const items = document.querySelector(".items");
 
 // List items - filter
 // Show picture on hover
+
+const data = jsonData;
 
 // User object constructor
 const User = function (username, password) {
@@ -29,6 +42,7 @@ loginButton.addEventListener("click", function (element) {
 	if (username.value === user1.username && password.value == user1.password) {
 		console.log("success");
 		document.querySelector(".login-container").classList.add("hidden");
+		document.querySelector('.checkout').classList.remove('hidden')
 		document.querySelector(".item-container").classList.remove("hidden");
 	} else {
 		alert("Incorrect username or password");
@@ -91,44 +105,85 @@ function createList(array) {
 		const supplier = document.createElement("li");
 		const type = document.createElement("li");
 		const use = document.createElement("li");
+		const button = document.createElement('button')
 
 		articles.classList.add("articles");
 		articles.style.fontWeight = "bold";
 		articles.innerHTML = array[i].artnr;
+
 		descriptions.innerHTML = array[i].description;
 		descriptions.classList.add("descriptions");
+		
 		supplier.innerHTML = array[i].supplier;
 		supplier.classList.add("supplier");
+		
 		type.innerHTML = array[i].type;
 		type.classList.add("type");
+		
 		use.innerHTML = array[i].use;
 		use.classList.add("use");
+		
+		button.classList.add('cart-button')
+		button.innerHTML = `<i class="fa-regular fa-plus"></i>`
+
 		div.appendChild(articles);
 		div.append(descriptions);
 		div.append(supplier);
 		div.append(type);
 		div.append(use);
+		div.append(button)
 		firstTab.appendChild(div);
+   
+		// add items to the cart function 
+			button.addEventListener('click', function() {
+				// create the elements
+				const cartDiv = document.createElement('div')
+				const cartArtnr = document.createElement('li')
+				const cartArtDescr = document.createElement('li')
+				const deleteButton = document.createElement('button')
+
+				cartDiv.classList.add('cart-div')
+				// create art number and append it - according to the event listener
+				cartArtnr.innerHTML = data[i].artnr;
+				cartArtnr.classList.add('cart-item')
+				// create description and append it - according to the event listener
+				cartArtDescr.innerHTML = data[i].description;
+				cartArtDescr.classList.add('cart-art-description')
+				// create the delete button and append it
+				deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`
+				deleteButton.classList.add('delete-button')
+
+				cartDiv.append(cartArtnr)
+				cartDiv.append(cartArtDescr)
+				cartDiv.append(deleteButton)
+				cartContainer.appendChild(cartDiv)
+
+				
+				console.log(data[i].artnr)
+
+				console.log(totalNumber.length)
+				// Generating the total
+					// const total = document.querySelector('.total')
+					// const totalArtikel = document. querySelector('.total-artikel')
+
+				
+
+					// console.log(cartContainer.length)
+
+			})
 	}
+
+	
 	itemsList.append(firstTab);
-
+	
+	
 	// itemsList.append(stringified)
+
+		// event listener for the add button
+		
 }
 
-// createList(jsonData)
 
-const dropdown = document.querySelectorAll(".dropdown");
-const itemsListContainer = document.querySelector(".item-list-container");
-
-for (let i = 0; i < dropdown.length; i++) {
-	dropdown[i].addEventListener("change", function () {
-		console.log(dropdown[i].value);
-
-		// const filteredData = jsonData.filter(function(filter) {
-
-		// })
-	});
-}
 
 // function createList(filteredarray) {
 // 	// selecting the item container
@@ -172,9 +227,7 @@ for (let i = 0; i < dropdown.length; i++) {
 
 // });
 
-const lieferantenDropdown = document.getElementById("lieferanten");
-const gewerkDropdown = document.getElementById("gewerk");
-const anwendungDropdown = document.getElementById("anwendung");
+
 
 // create an empty array
 const arraySuppliers = [];
@@ -192,6 +245,7 @@ const filterFunction = jsonData.forEach(function (e) {
 const newSetSuppliers = new Set(arraySuppliers);
 const newSetGewerk = new Set(arrayGewerk);
 const newSetAnwendung = new Set(arrayAnwendung);
+
 
 // loop through the array and for each element, create a new element - option - and add the inner HTML to the element (supplier) - and then append the element to the dropdown menu
 // this has to be optimised
@@ -218,7 +272,6 @@ newSetAnwendung.forEach(function (e) {
 	// console.log(newElement)
 	// console.log(e)
 	newElement.innerHTML = e;
-
 	anwendungDropdown.append(newElement);
 });
 
@@ -228,7 +281,7 @@ lieferantenDropdown.addEventListener("change", function (e) {
 	const filteredData = jsonData.filter(function (articles) {
 		return articles.supplier === lieferantenDropdown.value;
 	});
-	console.log(filteredData);
+	
 
 	// WORK ON THIS FUNCTION
 	// this empties the itemslistcontainer
@@ -242,11 +295,12 @@ lieferantenDropdown.addEventListener("change", function (e) {
 	}
 });
 
+// KLIMA does not show // TROCKENBAU not showing
 gewerkDropdown.addEventListener("change", function (e) {
 	const filteredData = jsonData.filter(function (articles) {
 		return articles.type === gewerkDropdown.value;
 	});
-	console.log(filteredData);
+	
 	items.innerHTML = "";
 	createList(filteredData);
 
@@ -256,11 +310,13 @@ gewerkDropdown.addEventListener("change", function (e) {
 	}
 });
 
+// error : BAD / HOLZ is not showing
 anwendungDropdown.addEventListener("change", function (e) {
 	const filteredData = jsonData.filter(function (articles) {
 		return articles.use === anwendungDropdown.value;
 	});
-	console.log(filteredData);
+	console.log(filteredData)
+	
 	items.innerHTML = "";
 	createList(filteredData);
 
@@ -271,7 +327,7 @@ anwendungDropdown.addEventListener("change", function (e) {
 });
 
 // Searchbox functionality
-const searchbox = document.getElementById("searchbox");
+
 searchbox.style.padding = "0 10px";
 searchbox.style.outline = "none";
 
@@ -280,9 +336,47 @@ searchbox.addEventListener("input", function (e) {
 	const searchdata = jsonData.filter(function (articles) {
 		return articles.description.toLowerCase().includes(searchbox.value);
 	});
-	console.log(searchdata);
 	// empty items list
 	items.innerHTML = "";
 	// create a list from the searched data and display it
 	createList(searchdata);
 });
+
+
+// Creating the cart
+
+// jsonData.forEach(function(e) {
+// 	cartButton.addEventListener('click', function() {
+// 		console.log('click')
+// 	})	
+	
+// })
+
+
+
+
+// cartButton.addEventListener('click', function addCartItems (e) { 	
+// 	// console.log(this.parentElement)
+// 	// let array = [];
+// 	// array =+ array.push(this.parentElement)
+// 	// console.log(array)
+
+	
+// const cartDiv = document.createElement('div');
+// const artnr = document.createElement('li')
+// const quantity = document.createElement('li');
+
+// cartDiv.classList.add('cart-div')
+// artnr.classList.add('cart-art')
+// artnr.style.listStyleType="none"
+// quantity.classList.add('cart-qty')
+
+// if (e.target) {
+// 	console.log();
+// }
+
+// cartDiv.append(artnr)
+// cart.appendChild(cartDiv)
+// });
+
+// createList(jsonData)
