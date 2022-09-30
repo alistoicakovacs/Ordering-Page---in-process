7d1548ec4a011ff0a5d65b109e69bc90f3cecfe4
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-09-30 08:57:08 +0200

    code restructured - assertion removed - safari / ios compatible

--- js/test.js
+++ js/test.js
@@ -1,6 +1,3 @@
-const test = "test";
-console.log(test);
-
 // function getData() {
 // 	return fetch("/js/convertcsv.json");
 // }
@@ -19,13 +16,20 @@ console.log(test);
 
 // console.log(getData());
 
-async function fetchAsync() {
-	var response = await fetch("./js/convertcsv.json");
-	window.data = await response.json();
+// async function fetchAsync() {
+// 	var response = await fetch("./js/convertcsv.json");
+// 	window.data = await response.json();
 
-	// console.log(response);
-	console.log(data);
-	return data;
+// 	// console.log(response);
+// 	console.log(data);
+// 	return data;
+
+async function getData(url) {
+	const response = await fetch("./js/convertcsv.json");
+
+	return response.json();
 }
 
-const jsonData = fetchAsync().then((data) => console.log(data));
+const data = await getData("./js/convertcsv.json");
+
+console.log({ data });
